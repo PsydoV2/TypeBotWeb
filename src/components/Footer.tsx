2903bf3b8af4368f811a17164144633feb23d0a1
Sebastian <seb.falter@gmail.com>
2025-07-24 21:30:22 +0200

    `Added Framer Motion library and updated various components to use it, including Hero, Navbar, and Functions components. Also updated package-lock.json and package.json to reflect the added dependency.`

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,49 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 
 export default function Footer() {
-    return <footer className="footer">
-        <div className="footerLeftSide">
-            <Image src="/bot.png" alt="TypeBot" height={100} width={100} />
-            <h4>TypeBot</h4>
-        </div>
-        <div className="footerRightSide">
-            <ul>
-                <li><a href="">Privacy Policy</a></li>
-                <li><a href="">Terms of Use</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-            <ul>
-                <li><a href="">Privacy Policy</a></li>
-                <li><a href="">Terms of Use</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-        </div>
+  return (
+    <footer className="footer">
+      <div className="footerLeftSide">
+        <Image
+          src="/typebotguy/typebotsummer.png"
+          alt="TypeBot"
+          height={100}
+          width={100}
+        />
+        <h4>TypeBot</h4>
+      </div>
+      <div className="footerRightSide">
+        <ul>
+          <li>
+            <a href="">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="">Terms of Use</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#start">Home</a>
+          </li>
+          <li>
+            <a href="#functions">Features</a>
+          </li>
+          <li>
+            <a href="https://discord.com/oauth2/authorize?client_id=1383532014267007046&permissions=564445071895638&integration_type=0&scope=bot">
+              Add to Discord
+            </a>
+          </li>
+        </ul>
+      </div>
 
-        <div className="footerBottom">
-            <h4>&copy; {new Date().getFullYear()} Sebastian Falter</h4>
-        </div>
+      <div className="footerBottom">
+        <h4>&copy; {new Date().getFullYear()} Sebastian Falter</h4>
+      </div>
     </footer>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,31 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import {FaGithub} from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
-    return <nav>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-            <Image src={"/icon.png"} alt={"Logo"} height={80} width={80}/>
-            <h1>TypeBot</h1>
-        </div>
-        <button onClick={() => window.open("https://github.com/PsydoV2/TypeBot", "_blank")} id="gitBtnSmall"> <FaGithub /></button>
+  return (
+    <nav>
+      <Image
+        src="/typebotguy/typebotsummer.png"
+        alt="TypeBot Logo"
+        width={50}
+        height={50}
+        priority
+      />
+
+      <h1>TypeBot</h1>
+
+      <motion.a
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        transition={{ type: "spring", stiffness: 300 }}
+        href="https://github.com/PsydoV2/TypeBot"
+        id="gitBtnSmall"
+      >
+        <FaGithub />
+      </motion.a>
     </nav>
+  );
 }
